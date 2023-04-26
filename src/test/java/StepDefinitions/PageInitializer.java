855253d7ec3606d8054faf6edeb32eae67ac7ddf
1CLC1 <ChrisC1L1C@gmail.com>
2023-04-26 18:44:51 -0400

    employee termination ability

--- src/test/java/StepDefinitions/PageInitializer.java
+++ src/test/java/StepDefinitions/PageInitializer.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Pages.EmployeeTerminationPage;
 import Pages.LoginPage;
 import net.bytebuddy.asm.Advice;
 
@@ -8,9 +9,10 @@ import java.sql.Driver;
 public class PageInitializer {
 
     public static LoginPage login;
+    public static EmployeeTerminationPage terminationPage;
 
     public static void PageObjectInitializer(){
-
+        terminationPage = new EmployeeTerminationPage();
         login = new LoginPage();
     }
 }
---
+++ src/test/resources/Features/EmployeeTermination.feature
@@ -0,0 +1,10 @@
+Feature: Employee Termination
+
+  @testcase
+  Scenario: Employee Termination Ability
+    Given admin user log in to the system using valid credentials
+    When user accesses the list of employees in the system
+    And select a specified employee from the list
+    And navigate to the employee's job page
+    And select the option to terminate the employment of the specified employee
+    Then specified employee is terminated
